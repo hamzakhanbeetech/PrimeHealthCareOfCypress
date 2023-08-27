3ea88dbd4d28332c7472269b44419f1c1696a51c
HamzaKhanBeetech <hkhan1241@gmail.com>
2023-08-27 12:28:18 +0500

    changed port

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.sendFile('/index.html', {root: __dirname })
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 443)
